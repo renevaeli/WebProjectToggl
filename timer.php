0a0625f84b321d5a8fece4311a65be555e3b1e3c
Rene VÄli <rene.vaeli@gmail.com>
2017-06-04 00:21:52 +0300

    timer css version 1 done

--- timer.php
+++ timer.php
@@ -2,26 +2,53 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>TogglTimer</title>
+    <link rel="stylesheet" href="css/w3.css">
     <script src="js/jquery-3.2.1.min.js"></script>
     <script src="js/toggleTimer.js"></script>
     <script src="js/functions.js"></script>
   </head>
   <body>
-    <div style="width: 600px; margin: auto;" align="center">
-      <div align="right"><button id="btnLogOut">Log Out</button></div>
-      <p></p>
-      <table>
+    <div class="w3-container">
+
+      <!-- Header and LogOut -->
+      <div class="w3-container w3-red" >
+        <div class="w3-container w3-threequarter">
+          <h2>Track your activities</h2>
+        </div>
+        <div class="w3-container w3-quarter">
+          <button class="w3-button w3-black w3-right" id="btnLogOut">Log Out</button>
+        </div>
+      </div>
+
+      <!-- Timer -->
+      <div class="w3-row-padding w3-padding-16">
+        <div class="w3-container w3-threequarter">
+          <input class="w3-input" type=text id="inputDescription" placeholder="Description">
+        </div>
+        <div class="w3-container w3-quarter">
+          <div class="w3-container w3-half">
+            <h3 class="w3-right" id="timer">00:00:00</h3>
+          </div>
+          <div class="w3-container w3-half">
+            <button class="w3-button w3-red" id="btnToggle">Start</button>
+          </div>
+        </div>
+      </div>
+
+      <!-- <table>
         <tr>
-          <td><input type=text id="inputDescription" placeholder="Description"></td>
-          <td><div id="timer">00:00:00</div></td>
-          <td><button id="btnToggle">Start</button></td>
+          <td></td>
+          <td></td>
+          <td></td>
         </tr>
       </table>
-      <p></p>
-      <table border="1">
+      <p></p> -->
+
+      <!-- Tracks table -->
+      <table class="w3-table-all w3-hoverable">
         <thead>
-          <tr><td>Description</td><td>Start</td><td>Stop</td><td>Duration</td></tr>
+          <tr class="w3-light-grey"><td>Description</td><td>Start</td><td>Stop</td><td>Duration</td></tr>
         </thead>
         <tbody id="trackData">
         </tbody>
